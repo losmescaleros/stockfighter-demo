04a5141c25e8017b5389b1f8800ed8bbf235faec
Mitchell Neville <mitcheln@Mitchells-MacBook-Pro.local>
2016-03-03 21:03:24 -0800

    Changed app.js to basic stream example

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var account = 'SB56812489';
-var venue = 'ETABEX';
-var symbol = 'POH';
+// Account, venue, and symbol will change with every problem session. Be sure to update them. 
+// The test index won't work with the stream... so you'll have to replace the account, venue, and 
+// symbol
+var account = 'SAP12306403';
+var venue = 'APEMEX';
+var symbol = 'ITBE';
 
 var WebSocketClient = require('websocket').client;
  
@@ -14,52 +17,25 @@ client.on('connectFailed', function(error) {
  
 client.on('connect', function(connection) {
     console.log('WebSocket Client Connected');
+
     connection.on('error', function(error) {
         console.log("Connection Error: " + error.toString());
     });
+
     connection.on('close', function() {
         console.log('Connection Closed');
     });
+
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             // parse the JSON object out of the utf8 string
             var data = JSON.parse(message.utf8Data);
             // JSON will contain 'quote' and 'ok'
             var quote = data.quote;
-            console.log(quote.last);
+            console.log(quote);
         }
-    });
-    
-    
+    });    
 });
+
  
-// client.connect('wss://api.stockfighter.io/ob/api/ws/' + account + '/venues/' + venue + '/tickertape', null);
-
-var requestLimitOrder = function(symbol, quantity, price, venue, account) {
-    var data = {      
-      "account" : account,
-      "venue": venue,
-      "stock": symbol,
-      "price": price,
-      "qty": quantity,
-      "direction": "buy",
-      "orderType": "limit",
-    };
-
-    var options = {
-        url: 'https://api.stockfighter.io/ob/api/venues/' + venue + '/stocks/' + symbol + '/orders',
-        headers: {
-            'X-Starfighter-Authorization': 'c86c741989ca560cdb9dbe52dfd2468d17980a11'
-        }, 
-        json: data
-    };
-    request.post(options, function(error, response, body) {
-        if(!error && response.statusCode == 200) {
-            console.log(body);
-        } else {
-            console.log(error);
-        }
-    });
-}
-
-requestLimitOrder(symbol, 100000, 8000, venue, account);
+client.connect('wss://api.stockfighter.io/ob/api/ws/' + account + '/venues/' + venue + '/tickertape', null);
